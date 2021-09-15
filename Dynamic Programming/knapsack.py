30dfbbe7370f22abb043604d346e9c2e02ff5d14
simrat_singh <simratsingh14@gmail.com>
2021-09-15 21:57:16 +0530

    passed n as input rather then array

--- Dynamic Programming/knapsack.py
+++ Dynamic Programming/knapsack.py
@@ -1,21 +1,29 @@
+import functools
+import time
 """
 A weight array and value array is given maximize the profit given a maximum weight list
 """
 
-def knapsackRecursive(weight,val,maximumWeight):
-    if len(weight) == 0 or maximumWeight == 0:
-        return 0
-    else:
-        w = weight.pop()
-        v = val.pop()
-        if maximumWeight < w:
-            return knapsackRecursive(weight,val,maximumWeight)
-        else:
-            print(v,w,val,weight,maximumWeight)
-            return max(v+knapsackRecursive(weight,val,maximumWeight-w),knapsackRecursive(weight,val,maximumWeight))
-
+weight = [10, 20, 30]
 val = [60, 100, 120]
-wt = [10, 20, 30]
 maximumWeight = 50
 
-print(knapsackRecursive(wt,val,maximumWeight))
+# @functools.cache
+def knapsackRecursive(n,maximumWeight):
+    if len(weight) == n or maximumWeight == 0:
+        return 0
+    else:
+        w = weight[n]
+        v = val[n]
+        if maximumWeight < w:
+            return knapsackRecursive(n+1,maximumWeight)
+        else:
+            return max(knapsackRecursive(n+1,maximumWeight),v+knapsackRecursive(n+1,maximumWeight-w))
+
+
+start = time.time()  
+print(knapsackRecursive(0,maximumWeight))
+end = time.time()
+print(f"time taken by recursive function {end-start}")  
+
+
