fc6bd3a3b0cbfbbe5ae8174e79feeb9567bb6c68
simrat_singh <simratsingh14@gmail.com>
2021-09-15 23:38:39 +0530

    added recusrive+memoization

--- Dynamic Programming/knapsack.py
+++ Dynamic Programming/knapsack.py
@@ -4,9 +4,9 @@ import time
 A weight array and value array is given maximize the profit given a maximum weight list
 """
 
-weight = [10, 20, 30]
-val = [60, 100, 120]
-maximumWeight = 50
+weight = [1,3,4,5]
+val = [1,4,5,7]
+maximumWeight = 7
 
 # @functools.cache
 def knapsackRecursive(n,maximumWeight):
@@ -26,4 +26,26 @@ print(knapsackRecursive(0,maximumWeight))
 end = time.time()
 print(f"time taken by recursive function {end-start}")  
 
+
+dar = {}
+def knapsackRecursiveMemoization(n,maximumWeight):
+    if (n,maximumWeight) in dar.keys():
+        return dar[(n,maximumWeight)] 
+    if len(weight) == n or maximumWeight == 0:
+        return 0
+    else:
+        w = weight[n]
+        v = val[n]
+        if maximumWeight < w:
+            dar[(n,maximumWeight)] = knapsackRecursive(n+1,maximumWeight) 
+            return dar[(n,maximumWeight)] 
+        else:
+            dar[(n,maximumWeight)] = max(knapsackRecursive(n+1,maximumWeight),v+knapsackRecursive(n+1,maximumWeight-w)) 
+            return dar[(n,maximumWeight)] 
+
+
+start = time.time()  
+print(knapsackRecursiveMemoization(0,maximumWeight))
+end = time.time()
+print(f"time taken by recursive+memoization function {end-start}")  
 
