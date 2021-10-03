a5aba1fdf835619f8225ad1a067ffe039967f536
simrat_singh <simratsingh14@gmail.com>
2021-10-03 14:31:56 +0530

    added bottom up approach

--- Lcs/lcs.py
+++ Lcs/lcs.py
@@ -7,18 +7,35 @@ For example, “abc”,  “abg”, “bdf”, “aeg”,  ‘”acefg”, .. et
 
 """
 
-str1 = "ABC"
-str2 = "AB"
+str1 = "ABCDGH"
+str2 = "AEDFHR"
 
 
-def lcs(str1,str2):
+def lcsRecursive(str1,str2):
     if str1 == '' or str2 == '':
         return 0
     else:
         if str2[-1] == str1[-1]:
-            return 1 + lcs(str1[:-1],str2[:-1])
+            return 1 + lcsRecursive(str1[:-1],str2[:-1])
         else:
-            return max(lcs(str1,str2[:-1]),lcs(str1[:-1],str2))
+            return max(lcsRecursive(str1,str2[:-1]),lcsRecursive(str1[:-1],str2))
 
-print(lcs(str1,str2))
+print(lcsRecursive(str1,str2))
+
+
+def lcsBottomUp(str1,str2):
+    m,n = len(str1),len(str2)
+    dp = [[0] +[-1]*(n) for i in range(m+1)]
+    dp[0] = [0]*(n+1)
+    for i in range(1,len(dp)):
+        for j in range(1,len(dp[0])):
+            if str1[i-1] == str2[j-1]:
+                dp[i][j] = 1 + dp[i-1][j-1]
+            else:
+                dp[i][j] = max(dp[i-1][j],dp[i][j-1])
+    # print(dp)
+    return dp[-1][-1]
+
+print(lcsBottomUp(str1,str2))
+
 
