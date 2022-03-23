12d6c4ba0dc5a15fbfb9680a7cb2dadfb243327a
Simrat Singh Chitkara <simratsingh14@gmail.com>
2022-03-23 22:50:04 +0530

    Time: 986 ms (39.37%), Space: 17.7 MB (23.42%) - LeetHub

---
+++ 542-01-matrix/542-01-matrix.py
@@ -0,0 +1,29 @@
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        queue = []
+        for i in range(len(mat)):
+            for j in range(len(mat[0])):
+                if not mat[i][j]:
+                    queue.append((i,j))
+                    
+        level = 0
+        dirs = [(-1,0),(1,0),(0,-1),(0,1)]
+        visited = set()
+        while queue:
+            size = len(queue)
+            for _ in range(size):
+                i,j = queue.pop(0)
+                for di in dirs:
+                    x = i + di[0] 
+                    y = j + di[1]
+                    if 0 <= x < len(mat) and 0 <= y < len(mat[0]) and (x,y) not in visited and mat[x][y] == 1:
+                        visited.add((x,y))
+                        mat[x][y] = level+1
+                        queue.append((x,y))
+                
+            
+            level+=1
+        return mat
+                    
+        
+        
