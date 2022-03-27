a589bcd8c3b48e51ff002a9bea852fa1314b009a
Simrat Singh Chitkara <simratsingh14@gmail.com>
2022-03-27 08:59:16 +0530

    Time: 137 ms (65.97%), Space: 14.3 MB (18.28%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.py
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.py
@@ -1,17 +1,8 @@
 class Solution:
     def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
         heap = []
-        def findSoldier(arr):
-            left,right = 0,len(arr)-1
-            while left  <= right:
-                mid = left + (right-left)//2
-                if arr[mid]:
-                    left = mid+1
-                else:
-                    right = mid-1
-            return left
         for i in range(len(mat)):
-            s = findSoldier(mat[i])
+            s = sum(mat[i])
             heapq.heappush(heap,(s,i))
         #print(heap)
         arr = []
