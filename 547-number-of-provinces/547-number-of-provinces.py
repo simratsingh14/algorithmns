96982cd220693b255be1ddaef95559384e3a07de
Simrat Singh Chitkara <simratsingh14@gmail.com>
2022-04-09 18:23:59 +0530

    Time: 224 ms (73.80%), Space: 14.5 MB (32.44%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.py
+++ 547-number-of-provinces/547-number-of-provinces.py
@@ -2,30 +2,27 @@ class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
         self.parent = [i for i in range(len(isConnected))]
         self.size = [1]*len(isConnected)
-        
-        def findParent(edge):
-            if self.parent[edge] == edge:
-                return edge
-            
-            par = self.parent[edge]
-            ans = findParent(par)
-            self.parent[edge] = ans
-            return ans
-        
+        def findParent(node):
+            if self.parent[node] == node:
+                return node
+            val = self.parent[node]
+            self.parent[node] = findParent(val)
+            return self.parent[val]
         for i in range(len(isConnected)):
             for j in range(len(isConnected[0])):
-                if i!=j and isConnected[i][j]:
-                    pu = findParent(i)
-                    pv = findParent(j)
+                if isConnected[i][j]:
+                    pu,pv = findParent(i),findParent(j)
                     if self.size[pu] > self.size[pv]:
-                        self.size[pu]+=self.size[pv]
-                        self.parent[pv] = pu
+                        self.size[pu] += self.size[pv]
+                        self.parent[pv] = self.parent[pu]
                     else:
-                        self.size[pv]+=self.size[pu]
-                        self.parent[pu] = pv
-        count = 0            
+                        self.size[pv] += self.size[pu]
+                        self.parent[pu] = self.parent[pv]
+        ans = 0
         for idx,val in enumerate(self.parent):
-            if idx == val:
-                count+=1
-        return count
+            if val == idx:
+                ans+=1
+        return ans
             
+                        
+        
