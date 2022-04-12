dfb5ea7b8000ef863e7d81db8c0a2e33126aa89a
Simrat Singh Chitkara <simratsingh14@gmail.com>
2022-04-12 22:37:12 +0530

    Time: 14003 ms (6.74%), Space: 15.1 MB (26.13%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.py
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -8,6 +8,8 @@ class Solution:
             for jump in range(nums[i]+1):
                 if i+jump < len(nums):
                     minVal = min(minVal,1+dp[i+jump])
+                else:
+                    break
             dp[i] = minVal
         #print(dp)
         return dp[0]
