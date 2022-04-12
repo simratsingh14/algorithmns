00da228e10de251de2f7d335167fdd6b50eb5764
Simrat Singh Chitkara <simratsingh14@gmail.com>
2022-04-12 22:43:55 +0530

    Time: 12348 ms (10.21%), Space: 14.6 MB (99.33%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.py
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -4,7 +4,9 @@ class Solution:
         dp[-1] = 0
         for i in range(len(nums)-2,-1,-1):
             #print(dp)
-            minVal = 10e4
+            minVal = 10001
+            if nums[i] == 0:
+                continue
             for jump in range(nums[i]+1):
                 if i+jump < len(nums):
                     minVal = min(minVal,1+dp[i+jump])
