1b02386833bdcbca6bb5db8c9c2f45d8b7b28267
Simrat Singh Chitkara <simratsingh14@gmail.com>
2022-04-27 00:04:07 +0530

    Time: 3637 ms (5.29%), Space: 16.1 MB (25.44%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,24 +1,19 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        @lru_cache(None)
-        def helper(inx,amount):
-            if amount == 0:
-                return 0
+        dp = [[0]*(amount+1) for _ in range(len(coins)+1)]
+        for i in range(1,len(dp[0])):
+            dp[0][i] = 100001
             
-            if inx == len(coins):
-                return 100000
-            
-            ans = 0
-            if amount >= coins[inx]:
-                ans = min(1+helper(inx,amount-coins[inx]),helper(inx+1,amount))
-            else:
-                ans = helper(inx+1,amount)
-            return ans
-        
-        if helper(0,amount) == 100000:
+        for inx in range(1,len(dp)):
+            for amt in range(1,len(dp[0])):
+                if amt >= coins[inx-1]:
+                    dp[inx][amt] = min(1+dp[inx][amt - coins[inx-1]],dp[inx-1][amt])
+                else:
+                    dp[inx][amt] = dp[inx-1][amt]
+        if dp[-1][-1] == 100001:
             return -1
-        else:
-            return helper(0,amount)
+        return dp[-1][-1]
+        
             
             
         
