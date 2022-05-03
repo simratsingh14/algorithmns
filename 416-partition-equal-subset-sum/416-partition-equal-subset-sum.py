9ad26feceef8eea05e90e38a3324cf769367725a
Simrat Singh Chitkara <simratsingh14@gmail.com>
2022-05-03 16:21:18 +0530

    Time: 1663 ms (46.65%), Space: 14.1 MB (81.85%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
@@ -3,15 +3,14 @@ class Solution:
         if sum(nums) % 2 == 1:
             return False
         total = sum(nums)//2
-        dp = [[True] + [False]*total for _ in range(len(nums)+1)]
+        dp = [True] + [False]*total
         #print(dp)
-        for i in range(1,len(dp)):
-            for j in range(1,len(dp[0])):
-                if j >= nums[i-1]:
-                    dp[i][j] = dp[i-1][j-nums[i-1]] or dp[i-1][j]
-                else:
-                    dp[i][j] = dp[i-1][j]
+        for i in nums:
+            #print(dp)
+            for j in range(len(dp)-1,0,-1):
+                if j >= i:
+                    dp[j] = dp[j-i] or dp[j]
         #print(dp)
-        return dp[-1][-1]
+        return dp[-1]
                 
         
