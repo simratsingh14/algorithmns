98fe3e95d221b66e747b7e64441f0f882243276c
Simrat Singh Chitkara <simratsingh14@gmail.com>
2022-05-09 23:58:18 +0530

    Time: 424 ms (83.75%), Space: 22 MB (66.97%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,13 +1,22 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        @lru_cache(None)
-        def recursive(s,t):
-            if s == 0 or  t == 0:
-                return 0
-            if text1[s-1] == text2[t-1]:
-                return 1 + recursive(s-1,t-1)
-            else:
-                return max(recursive(s-1,t),recursive(s,t-1))
-        return recursive(len(text1),len(text2))
+        # @lru_cache(None)
+        # def recursive(s,t):
+        #     if s == 0 or  t == 0:
+        #         return 0
+        #     if text1[s-1] == text2[t-1]:
+        #         return 1 + recursive(s-1,t-1)
+        #     else:
+        #         return max(recursive(s-1,t),recursive(s,t-1))
+        # return recursive(len(text1),len(text2))
             
+        n,m = len(text1),len(text2)
+        dp = [[0]*(m+1) for _ in range(n+1)]
+        for i in range(1,len(dp)):
+            for j in range(1,len(dp[0])):
+                if text1[i-1] == text2[j-1]:
+                    dp[i][j] = 1 + dp[i-1][j-1]
+                else:
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1])
+        return dp[-1][-1]
         
