4637bae8cc233c2e37a7d7345f7dd2a9b65accdd
Simrat Singh Chitkara <simratsingh14@gmail.com>
2022-05-10 22:16:28 +0530

    Time: 476 ms (70.74%), Space: 13.7 MB (99.91%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -10,13 +10,28 @@ class Solution:
         #         return max(recursive(s-1,t),recursive(s,t-1))
         # return recursive(len(text1),len(text2))
             
+        # n,m = len(text1),len(text2)
+        # dp = [[0]*(m+1) for _ in range(n+1)]
+        # for i in range(1,len(dp)):
+        #     for j in range(1,len(dp[0])):
+        #         if text1[i-1] == text2[j-1]:
+        #             dp[i][j] = 1 + dp[i-1][j-1]
+        #         else:
+        #             dp[i][j] = max(dp[i-1][j],dp[i][j-1])
+        # return dp[-1][-1]
+        
         n,m = len(text1),len(text2)
-        dp = [[0]*(m+1) for _ in range(n+1)]
-        for i in range(1,len(dp)):
-            for j in range(1,len(dp[0])):
+        prev,curr = [0]*(m+1),[0]*(m+1)
+        for i in range(1,n+1):
+            #print(prev,curr)
+            for j in range(1,m+1):
                 if text1[i-1] == text2[j-1]:
-                    dp[i][j] = 1 + dp[i-1][j-1]
+                    curr[j] = 1 + prev[j-1]
                 else:
-                    dp[i][j] = max(dp[i-1][j],dp[i][j-1])
-        return dp[-1][-1]
+                    curr[j] = max(prev[j],curr[j-1])
+            prev = curr[:]
+        #print(prev,curr)
+        return prev[-1]
+            
+        
         
