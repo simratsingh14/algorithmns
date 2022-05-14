a1fb4f9abfb69295b5e2aa5e72071ab205e68e4a
Simrat Singh Chitkara <simratsingh14@gmail.com>
2022-05-14 17:03:47 +0530

    Time: 877 ms (53.81%), Space: 143.3 MB (10.35%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.py
+++ 44-wildcard-matching/44-wildcard-matching.py
@@ -2,21 +2,22 @@ class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         @lru_cache(None)
         def helper(l1,l2):
-            if l1 == len(s) and l2 == len(p):
+            if l1 == 0 and l2 == 0:
                 return True
-            elif len(p) == l2:
+            elif l2 == 0:
                 return False
-            elif len(s) == l1 and p[l2] != '*':
+            elif l1 == 0 and p[l2-1] != '*':
                 return False
             
-            if p[l2] == '*':
-                if len(s) == l1:
-                    return helper(l1,l2+1)
-                return helper(l1+1,l2) or helper(l1+1,l2+1) or helper(l1,l2+1)
-            elif p[l2] == '?':
-                return helper(l1+1,l2+1)
+            if p[l2-1] == '*':
+                if l1 == 0:
+                    return helper(l1,l2-1)
+                return helper(l1-1,l2) or helper(l1-1,l2-1) or helper(l1,l2-1)
+            elif p[l2-1] == '?':
+                return helper(l1-1,l2-1)
             else:
-                return s[l1] == p[l2] and helper(l1+1,l2+1)
-        return helper(0,0)
+                return s[l1-1] == p[l2-1] and helper(l1-1,l2-1)
+        return helper(len(s),len(p))
+            
             
         
