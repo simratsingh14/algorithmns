de0b2f05824470d67bcee9be3a2c1171bbd1532d
Simrat Singh Chitkara <simratsingh14@gmail.com>
2022-05-14 16:12:40 +0530

    Time: 220 ms (46.68%), Space: 14 MB (95.32%) - LeetHub

--- 72-edit-distance/72-edit-distance.py
+++ 72-edit-distance/72-edit-distance.py
@@ -10,15 +10,29 @@ class Solution:
             else:
                 return min(1+recursive(s-1,t-1),1+recursive(s-1,t),1+recursive(s,t-1))
         #return recursive(len(word1),len(word2))
-        dp = [[0]*(len(word1)+1) for _ in range(len(word2)+1)]
-        for i in range(len(dp)):
-            for j in range(len(dp[0])):
-                if i == 0 or j == 0:
-                    dp[i][j] = max(i,j)
-                elif word1[j-1] == word2[i-1]:
-                    dp[i][j] = dp[i-1][j-1]
+        # dp = [[0]*(len(word1)+1) for _ in range(len(word2)+1)]
+        # for i in range(len(dp)):
+        #     for j in range(len(dp[0])):
+        #         if i == 0 or j == 0:
+        #             dp[i][j] = max(i,j)
+        #         elif word1[j-1] == word2[i-1]:
+        #             dp[i][j] = dp[i-1][j-1]
+        #         else:
+        #             dp[i][j] = 1 + min(dp[i][j-1],dp[i-1][j-1],dp[i-1][j])
+        # return dp[-1][-1]
+        curr = [i for i in range(len(word1)+1)]
+        prev = curr[:]
+        for i in range(1,len(word2)+1):
+            for j in range(len(curr)):
+                if j == 0:
+                    curr[j] = i
+                elif word2[i-1] == word1[j-1]:
+                    curr[j] = prev[j-1]
                 else:
-                    dp[i][j] = 1 + min(dp[i][j-1],dp[i-1][j-1],dp[i-1][j])
-        return dp[-1][-1]
+                    curr[j] = 1 + min(prev[j-1],prev[j],curr[j-1])
+            prev = curr[:]
+        return prev[-1]
+                    
+        
             
         
