f922c2a931f7b3980d9d5ef48e0deb1e33b8f056
Simrat Singh Chitkara <simratsingh14@gmail.com>
2022-05-14 16:03:40 +0530

    Time: 283 ms (25.04%), Space: 17.6 MB (64.16%) - LeetHub

--- 72-edit-distance/72-edit-distance.py
+++ 72-edit-distance/72-edit-distance.py
@@ -3,14 +3,22 @@ class Solution:
         @lru_cache(None)
         def recursive(s,t):
             #print(s,t)
-            if s == 0  and t == 0:
-                return 0
-            elif s == 0 or t == 0:
+            if s == 0 or t == 0:
                 return max(s,t)
             if word1[s-1] == word2[t-1]:
                 return recursive(s-1,t-1)
             else:
                 return min(1+recursive(s-1,t-1),1+recursive(s-1,t),1+recursive(s,t-1))
-        return recursive(len(word1),len(word2))
+        #return recursive(len(word1),len(word2))
+        dp = [[0]*(len(word1)+1) for _ in range(len(word2)+1)]
+        for i in range(len(dp)):
+            for j in range(len(dp[0])):
+                if i == 0 or j == 0:
+                    dp[i][j] = max(i,j)
+                elif word1[j-1] == word2[i-1]:
+                    dp[i][j] = dp[i-1][j-1]
+                else:
+                    dp[i][j] = 1 + min(dp[i][j-1],dp[i-1][j-1],dp[i-1][j])
+        return dp[-1][-1]
             
         
