75225cd023b789273c094dbc4611d1b80adc3f19
Simrat Singh Chitkara <simratsingh14@gmail.com>
2022-05-14 17:19:27 +0530

    Time: 892 ms (52.62%), Space: 22.4 MB (66.22%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.py
+++ 44-wildcard-matching/44-wildcard-matching.py
@@ -17,7 +17,23 @@ class Solution:
                 return helper(l1-1,l2-1)
             else:
                 return s[l1-1] == p[l2-1] and helper(l1-1,l2-1)
-        return helper(len(s),len(p))
+        #return helper(len(s),len(p))
+        dp = [[False]*(len(s)+1) for _ in range(len(p)+1)]
+        dp[0][0] = True
+        for i in range(1,len(dp)):
+             if p[i-1] == '*':
+                dp[i][0] = dp[i-1][0]
+        for i in range(1,len(dp)):
+            for j in range(1,len(dp[0])):
+                if p[i-1] == '*':
+                    dp[i][j] = dp[i-1][j-1] or dp[i-1][j] or dp[i][j-1]
+                elif p[i-1] == '?':
+                    dp[i][j] = dp[i-1][j-1]
+                else:
+                    dp[i][j] = p[i-1] == s[j-1] and dp[i-1][j-1]
+        return dp[-1][-1]
+                
+        
             
             
         
