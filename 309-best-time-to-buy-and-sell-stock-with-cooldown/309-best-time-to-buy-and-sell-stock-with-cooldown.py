6e08ef7334f04081fc764078f9e744c7fe5e7a43
Simrat Singh Chitkara <simratsingh14@gmail.com>
2022-05-23 14:02:00 +0530

    Time: 60 ms (53.70%), Space: 14 MB (96.51%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,12 +1,14 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        if len(prices) < 2:
-            return 0
-        s1 = -prices[0]     # profit after buying
-        s2 = 0              # profit after selling
-        s0 = [0, 0]         # cooldown profit using a queue
-        for p in prices:
-            s1 = max(s1, s0[1] - p)
-            s2 = max(s2, s1 + p)
-            s0 = [s2, s0[0]]
-        return s2
+        buying = -prices[0]
+        profit = 0
+        prev = 0
+        for i in range(1,len(prices)):
+            #print(prev,profit,buying)
+            temp = profit
+            profit = max(profit,buying+prices[i])
+            buying = max(buying,prev-prices[i])
+            prev = temp
+        #print(profit,prev,buying)
+        return max(prev,profit)
+        
