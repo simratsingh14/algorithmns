3f400e0f9561efc3d510e249687cc899e1372f88
Simrat Singh Chitkara <simratsingh14@gmail.com>
2022-05-29 15:59:46 +0530

    Time: 860 ms (62.50%), Space: 14.3 MB (71.68%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.py
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.py
@@ -2,11 +2,13 @@ class Solution:
     def maxProduct(self, words: List[str]) -> int:
         #print(set(words[0]).intersection(set(words[1])))
         ans = 0
+        d = defaultdict(int)
+        for word in words:
+            for l in word:
+                d[word] |= 1 << (ord(l)-ord('a'))
         for i in range(len(words)):
-            for j in range(i,len(words)):
-                inter = set(words[i]).intersection(set(words[j]))
-                if len(inter) == 0:
+            for j in range(i+1,len(words)):
+                if d[words[i]] & d[words[j]] == 0:
                     ans = max(ans,len(words[i])*len(words[j]))
         return ans
-                
         
