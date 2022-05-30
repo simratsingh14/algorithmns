b6ef04f811be2eacd3d764d0875b25bc78a68a90
Simrat Singh Chitkara <simratsingh14@gmail.com>
2022-05-31 01:06:48 +0530

    Time: 56 ms (27.40%), Space: 13.9 MB (64.08%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -4,16 +4,15 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         p1,p2,p3 = -1,0,len(nums)-1
-        
         while p2 <= p3:
             if nums[p2] == 0:
                 p1+=1
-                nums[p2],nums[p1] = nums[p1],nums[p2]
+                nums[p1],nums[p2] = nums[p2],nums[p1]
                 p2+=1
-            elif nums[p2] == 1:
-                p2+=1
-            else:
+            elif nums[p2] == 2:
                 nums[p3],nums[p2] = nums[p2],nums[p3]
                 p3-=1
+            else:
+                p2+=1
         
-        
+                
