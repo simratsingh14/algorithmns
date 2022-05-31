a0d743d54fd09d75c63fe9e8f70ac633910e3534
Simrat Singh Chitkara <simratsingh14@gmail.com>
2022-05-31 10:08:32 +0530

    Time: 217 ms (24.42%), Space: 14.8 MB (16.33%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.py
@@ -1,18 +1,29 @@
 class Solution:
-    def setZeroes(self, matrix: List[List[int]]) -> None:
-        """
-        Do not return anything, modify matrix in-place instead.
-        """
-        row,col = set(),set()
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-                if matrix[i][j] == 0:
-                    row.add(i)
-                    col.add(j)
-        
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-                if i in row or j in col:
-                    matrix[i][j] = 0
-        
-        
+	def setZeroes(self, matrix: List[List[int]]) -> None:
+		"""
+		Do not return anything, modify matrix in-place instead.
+		"""
+		
+		isCol = False
+		for i in range(len(matrix)):
+			if matrix[i][0] == 0:
+				isCol = True
+			for j in range(1,len(matrix[0])):
+				if not matrix[i][j]:
+					matrix[i][0] = 0
+					matrix[0][j] = 0
+		for i in range(1,len(matrix)):
+			for j in range(1,len(matrix[0])):
+				if not matrix[i][0] or not matrix[0][j]:
+					matrix[i][j] = 0
+		if not matrix[0][0]:
+			for j in range(len(matrix[0])):
+				matrix[0][j] = 0
+		if isCol:
+			for i in range(len(matrix)):
+				matrix[i][0] = 0
+			
+			
+	
+					
+		
